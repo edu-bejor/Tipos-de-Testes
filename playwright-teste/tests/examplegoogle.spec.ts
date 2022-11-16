9a719c35ff8381262f96192d4bcdff3961a4ceba
José Eduardo Bernardino Jorge <74507357+edu-bejor@users.noreply.github.com>
2022-11-16 15:54:51 -0300

    Create examplegoogle.spec.ts

---
+++ playwright-teste/tests/examplegoogle.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+import { chromium } from '@playwright/test';
+
+test('homepage has title and links to intro page', async ({ page:any }) => {
+  const browser = await chromium.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto('https://google.com');
+  const input = await page.locator("input[class='gLFyf gsfi']");
+  await input.fill('MackLeaps');
+  await page.waitForTimeout(3000);
+
+});
