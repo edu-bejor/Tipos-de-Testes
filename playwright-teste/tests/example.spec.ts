c5802eb4c6587fd40570fe16eb2a7e4dca3b17b1
José Eduardo Bernardino Jorge <74507357+edu-bejor@users.noreply.github.com>
2022-11-16 15:54:19 -0300

    Update example.spec.ts

--- playwright-teste/tests/example.spec.ts
+++ playwright-teste/tests/example.spec.ts
@@ -6,9 +6,14 @@ test('homepage has title and links to intro page', async ({ page:any }) => {
     headless: false,
   });
   const page = await browser.newPage();
-  await page.goto('https://google.com');
-  const input = await page.locator("input[class='gLFyf gsfi']");
-  await input.fill('MackLeaps');
+  await page.goto('https://github.com/login');
+  const inputemail = await page.locator("input[class='form-control input-block js-login-field']");
+  await inputemail.fill('levtruts@gmail.com');
+  await page.waitForTimeout(3000);
+  const inputsenha = await page.locator("input[class='form-control form-control input-block js-password-field']");
+  await inputsenha.fill('lfs123!@#');
+  await page.waitForTimeout(3000);
+  await page.click("input[class='btn btn-primary btn-block js-sign-in-button']");
   await page.waitForTimeout(3000);
 
 });
