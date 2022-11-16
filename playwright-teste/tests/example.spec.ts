a69f5fa6774f1a7a4aae6ad818590ac53930fa58
José Eduardo Bernardino Jorge <74507357+edu-bejor@users.noreply.github.com>
2022-11-16 15:32:48 -0300

    Update example.spec.ts

--- playwright-teste/tests/example.spec.ts
+++ playwright-teste/tests/example.spec.ts
@@ -9,6 +9,6 @@ test('homepage has title and links to intro page', async ({ page:any }) => {
   await page.goto('https://google.com');
   const input = await page.locator("input[class='gLFyf gsfi']");
   await input.fill('MackLeaps');
-  console.log(await input.textContent())
+  await page.waitForTimeout(3000);
 
 });
